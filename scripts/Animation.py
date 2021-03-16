c536955535bfa649cd3aaecf847c231dd4438a07
Lukas Sabaliauskas <lukas_sabaliauskas@hotmail.com>
2021-03-16 16:19:41 +0100

    J'ai une des trois comportement du boid (la séparation) mais seulement entre deux objets.
    Signed-off-by: Lukas Sabaliauskas <lukas_sabaliauskas@hotmail.com>

--- scripts/Animation.py
+++ scripts/Animation.py
@@ -2,7 +2,7 @@
 import maya.cmds as cmds
 import maya.internal.common.cmd.base
 
-#--------------------- ANIMATION DEFORMEUR ---------------------
+#_______________________ ANIMATION DEFORMEUR _______________________#
 
 #Variable globale avec le nom du deformeur
 nDeform = "waveDeform"
@@ -31,4 +31,58 @@ def boutonAnimerVagues(grpAnimVague, amplitude=0.05, maxRadius=1.5, lenteur=100)
     deformation(amplitude, maxRadius)
     animerDeformation(lenteur)
 
-#--------------------- ANIMATION BOIDS ---------------------
+#_______________________ ANIMATION BOIDS _______________________#
+cmds.file(new=True, f=True)
+import math
+
+#------scene init------#
+def getPos(obj):
+    return (cmds.xform(obj, q=True, t=True, ws=True))
+
+def getDis(pA, pB):
+    dis = math.sqrt( abs( pow((pB[0]-pA[0]), 2) + pow((pB[1]-pA[1]), 2) + pow((pB[2]-pA[2]), 2) ) )
+    return dis
+
+def getNormVec(pA, pB):
+    vecX = abs(pA[0] - pB[0])
+    vecY = abs(pA[1] - pB[1])
+    vecZ = abs(pA[2] - pB[2])
+    dis = getDis(pA, pB)
+    vecX /= dis
+    vecY /= dis
+    vecZ /= dis
+    return(vecX, vecY, vecZ, dis)
+
+def boidSep(sujet, proche):
+    pA = getPos(sujet)
+    pB = getPos(proche)
+    if(getDis(pA, pB) < disTol):
+        dirAB = getNormVec(pA, pB)
+        cmds.move(-dirAB[0]*vit/dirAB[3], -dirAB[1]*vit/dirAB[3], -dirAB[2]*vit/dirAB[3], sujet, r=True)
+        print("dirAB = " +str(dirAB))
+    else:
+        print("trop loin")
+
+def simulerBoids(sujet, proche, temps):
+    temps = int(temps)
+    for i in range(1, temps+1):
+        cmds.currentTime(i)
+        cmds.setKeyframe(sujet, at=["tx", "ty", "tz"], t=i)
+
+        boidSep(sujet, proche)
+
+#------scene init------#
+vit = 0.5
+disTol = 20
+rSph = cmds.polySphere()
+cmds.move(0, 0, 1, rSph, a=True)
+rCub = cmds.polyCube()
+cmds.move(3, 0, 4, rCub, a=True)
+
+# print("Distance = " +str(getDis(getPos(rSph), getPos(rCub))))
+# print(getNormVec(getPos(rSph), getPos(rCub)))
+
+simulerBoids(rSph, rCub, 10)
+
+
+
